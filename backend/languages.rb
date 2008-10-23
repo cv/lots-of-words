95b05b2b8e5a729e0bb902b3d49b0ea7b7010e72
Carlos Villela <cv@lixo.org>
2008-10-23 21:59:57 +0100

    added some doc

--- backend/languages.rb
+++ backend/languages.rb
@@ -1,9 +1,5 @@
 class Languages < Application
   
-  # match('/:language').            to(:controller => "languages",     :action => 'counts').     name(:counts)
-  # match('/:source/:target').      to(:controller => "languages",     :action => 'link_counts').name(:link_counts)
-  # match('/:source/:target/:term').to(:controller => "languages",     :action => 'link').       name(:links)
-
   def counts
     render
   end
