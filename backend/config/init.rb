9e62963fb9db1d42c22e7006f16a33697c73e348
Carlos Villela <cv@lixo.org>
2008-10-26 03:05:53 +0000

    yay, something that looks just as crap as the json itself! :D

--- backend/config/init.rb
+++ backend/config/init.rb
@@ -6,10 +6,9 @@ require 'couchrest'
 require 'application'
 require 'lots_of_words'
 require 'languages'
-require 'models'
 
 Merb::BootLoader.before_app_loads do
-  $couchdb = CouchRest.new('http://localhost:5984')
+  $couchdb = CouchRest.new('http://lotsofwords.com:5984')
   $lexicon = $couchdb.database('lexicon')
 end
  
--- backend/languages.rb
+++ backend/languages.rb
@@ -1,15 +1,27 @@
 class Languages < Application
   
+  provides :json, :xml
+  
   def counts
-    render
+    @counts = $lexicon.view('langs/count', {:startkey => params[:language], :group => true, :count => 1})
+
+    display @counts
   end
   
   def link_counts
-    render
+    @link_counts = $lexicon.view('langs/by_target_lang', :startkey => [params[:source], params[:target]].to_json, :count => 1)
+    
+    display @link_counts
   end
   
   def link
-    render
+    @link = $lexicon.view('langs/by_source_and_target', {
+      :startkey => [params[:source], params[:target], params[:term]],
+      :count => 1,
+      :include_docs => true,
+    })
+    
+    display @link
   end
   
 end
