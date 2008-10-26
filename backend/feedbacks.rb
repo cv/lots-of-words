ce59a131abe88b0b87f339191e758e4d132ba017
Carlos Villela <cv@lixo.org>
2008-10-26 15:23:24 +0000

    hmm, interesting problem with to_json

--- backend/feedbacks.rb
+++ backend/feedbacks.rb
@@ -1,7 +1,10 @@
 class Feedbacks < Application
   
   def create
-    $couchdb.database('feedback').save({ :uri => request.uri, :comment => params[:comment], :created_at => Time.now })
+    $couchdb.database('feedback').save(
+      :uri => request.uri,
+      :comment => params[:comment],
+    )
     redirect request.uri
   end
   
