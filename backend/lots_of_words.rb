0a54cda57b457d1d63675763032ba38ee042cc56
Carlos Villela <cv@lixo.org>
2008-11-02 02:22:00 +0000

    added a crappy little search bar

--- backend/lots_of_words.rb
+++ backend/lots_of_words.rb
@@ -1,6 +1,13 @@
 class LotsOfWords < Application
   
   def index
+    if params[:source] && params[:target] && params[:term]
+      redirect url(:link, {
+        :source => params[:source],
+        :target => params[:target],
+        :term => params[:term],
+      }), :permanent => true
+    end
     render
   end
   
